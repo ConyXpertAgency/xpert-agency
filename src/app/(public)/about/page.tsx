558b360fca0695fd6cc8b498fa1920386dadc914
gatoJijen <gato.jijen01@gmail.com>
2026-08-09 00:33:42 -0500

    about 1/2

---
+++ src/app/(public)/about/page.tsx
@@ -0,0 +1,12 @@
+import { First } from '@/components/about'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='AppShell'>
+        <First/>
+    </main>
+  )
+}
+
+export default page
