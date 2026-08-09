6d1b0d679951b1e4f7e28004717f00c3df9fb162
gatoJijen <gato.jijen01@gmail.com>
2026-08-09 12:55:32 -0500

    about 100%

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,10 +1,11 @@
-import { First } from '@/components/about'
+import { First, Second } from '@/components/about'
 import React from 'react'
 
 const page = () => {
   return (
     <main className='AppShell'>
         <First/>
+        <Second />
     </main>
   )
 }
